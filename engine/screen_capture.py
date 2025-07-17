32dafcdd3e9cfcd2565498e373ebcecba1fa0b0f
calisanemre <calisanemre@gmail.com>
2025-07-17 16:39:27 +0300

    first commit of ScreenCapture

---
+++ engine/screen_capture.py
@@ -0,0 +1,35 @@
+import mss
+import cv2
+import numpy as np
+
+class ScreenCapturer:
+    def __init__(self, monitor_index = 1):
+        """
+        Monitor Index:
+            0 = all screens combined
+            1 = primary screen
+            2 = secondary screen
+            ...
+        """
+        self.monitor_index = monitor_index
+        self.mss_instance = mss.mss()
+
+    def capture(self):
+        try:
+            monitor = self.mss_instance.monitors[self.monitor_index]
+            screenshot = self.mss_instance.grab(monitor)
+            img = np.array(screenshot)[:, :, :3]
+            return img
+        except Exception as e:
+            print(f"[ScreenCapturer/capture] -> {e}")
+            return None
+        
+    def preview(self):
+        frame = self.capture()
+        if frame is not None:
+            cv2.imshow("Screenshot", frame)
+            cv2.waitKey(0)
+            cv2.destroyAllWindows()
+        else:
+            print("[ScreenCapturer/preview] Error!")
+        
