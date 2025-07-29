08c86783cd3fc31eaba7f92ab76d37f9dd6b2a8b
calisanemre <calisanemre@gmail.com>
2025-07-29 14:01:19 +0300

    started using logging

---
+++ tools/logger.py
@@ -0,0 +1,27 @@
+import logging
+import os
+
+def setup_logger(name: str = "AmbilightLogger", log_file: str = "logs/ambilight.log", level=logging.DEBUG) -> logging.Logger:
+    # Klasör varsa oluştur
+    os.makedirs(os.path.dirname(log_file), exist_ok=True)
+
+    logger = logging.getLogger(name)
+    logger.setLevel(level)
+
+    # Aynı logger tekrar eklenmesin
+    if not logger.hasHandlers():
+
+        # Format
+        formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+
+        # Console handler
+        console_handler = logging.StreamHandler()
+        console_handler.setFormatter(formatter)
+        logger.addHandler(console_handler)
+
+        # File handler
+        file_handler = logging.FileHandler(log_file, mode='a', encoding='utf-8')
+        file_handler.setFormatter(formatter)
+        logger.addHandler(file_handler)
+
+    return logger
